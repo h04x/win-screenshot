cac9fab36ddb8700e63b5293004fd70c38dcdad8
h04x <liquid.doctrine@gmail.com>
2023-02-24 09:59:08 +0300

    add WSError to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,4 @@
 pub use super::{
-    capture::{capture_display, capture_window, Area, RgbaBuf},
+    capture::{capture_display, capture_window, Area, RgbaBuf, WSError},
     utils::{find_window, window_list, FWError, HwndName, WLError},
 };
